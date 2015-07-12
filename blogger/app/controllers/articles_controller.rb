4110a0129192f7fc0a9eef131f0e13aaf5c3a948
c1505 <coreym247@gmail.com>
2015-07-12 19:10:40 +0000

    second commit

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -2,4 +2,13 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
