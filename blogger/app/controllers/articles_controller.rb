f2b7d53353eab27d655bf918edecb43056751971
c1505 <coreym247@gmail.com>
2015-07-13 04:19:01 +0000

    form-based workflow feature completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+    
     def index
         @articles = Article.all
     end
@@ -7,8 +9,52 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
-    private
-        def article_params
-            params.require(:article).permit(:title, :text)
-        end
+    def new
+        @article = Article.new
+    end
+    
+    def create 
+        @article = Article.new(article_params)
+        @article.save
+        
+        flash.notice = "Article '#{@article.title}' Created"
+        
+        redirect_to article_path(@article)
+    end
+    
+    def destroy
+        @article = Article.find(params[:id]).destroy
+        
+        flash.notice = "Article '#{@article.title}' Deleted"
+        redirect_to articles_path
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        
+        flash.notice = "Article '#{@article.title}' Updated"
+        
+        redirect_to articles_path(@article)
+    end
 end
+
+#using this works without a private method because you are explicitly saying the params accepted 
+# def create
+#   @article = Article.new(
+#     title: params[:article][:title],
+#     body: params[:article][:body])
+#   @article.save
+#   redirect_to article_path(@article)
+# end
+
+
+
+
+
+
+
